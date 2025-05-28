db87c280b2340725724742401d52947b93e931ce
problemo <problemo@problemo.pro>
2025-05-29 00:33:50 +0200

    Cleanup and defined input/output files at the top.

--- library.c
+++ library.c
@@ -1,4 +1,6 @@
 #include "library.h"
+
+#include <math.h>
 #include <stdio.h>
 #include <processthreadsapi.h>
 #include <string.h>
@@ -6,42 +8,31 @@
 
 #define spoof_handle 41
 
+#define RAIL_DRIVER_INPUT_FILE "raildriver_input"
+#define LOG_FILE "raildriver_emulator_log.txt"
+#define LEDS_FILE "raildriver_leds.txt"
+
 PHIDDataEvent data_callback = NULL;
 PHIDErrorEvent error_callback = NULL;
 
 int _CRT_INIT(int dll_handle, int reason, longlong reserviert);
 
 void logtext(char* text) {
-    FILE* file = fopen("log.txt", "a");
-    fprintf(file, "%s\n", text);
-    fclose(file);
-}
-void logwrite(char* text) {
-    FILE* file = fopen("write.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "%s\n", text);
     fclose(file);
 }
 
 unsigned long data_thread(void* arg) {
     char data[16];
-    char last_data[16];
     while (data_callback != NULL) {
-        FILE* file = fopen("input", "r");
+        FILE* file = fopen(RAIL_DRIVER_INPUT_FILE, "r");
         fread(data, 1, 15, file);
         fclose(file);
         data[14] = RAIL_DRIVER_ID;
         data[15] = '\0';
 
         data_callback((unsigned char*)data, spoof_handle, 0);
-        // for (int i = 0; i < 15; i++) {
-        //     if (last_data[i] != data[i]) {
-        //         logtext(data);
-        //         data_callback((unsigned char*)data, spoof_handle, 0);
-        //         break;
-        //     }
-        // }
-
-        memcpy(last_data, data, 15);
         Sleep(10);
     }
     return 0;
@@ -96,7 +87,7 @@ char led2char(unsigned char led) {
         return '-';
     }
 
-    FILE* file = fopen("log.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "l: %d\n", led);
     fclose(file);
 
@@ -137,28 +128,28 @@ bool IsDataCallbackDisabled(const long handle) {
     return false;
 }
 
-bool GetSuppressDuplicateReports(long handle) {
-    FILE* file = fopen("log.txt", "a");
+bool GetSuppressDuplicateReports(const long handle) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "GetSuppressDuplicateReports param_1: %ld\n", handle);
     fclose(file);
     return true;
 }
 
-void GetErrorString(int errorNumber, char* errorString, int size) {
-    FILE* file = fopen("log.txt", "a");
+void GetErrorString(const int errorNumber, char* errorString, const int size) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "GetErrorString param_1: %d, param_2: %p, param_3: %d\n", errorNumber, errorString, size);
     fclose(file);
 }
 
-int GetXKeyVersion(long handle) {
-    FILE* file = fopen("log.txt", "a");
+int GetXKeyVersion(const long handle) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "GetXKeyVersion param_1: %ld\n", handle);
     fclose(file);
     return 0xffff;
 }
 
-int ReadLast(long handle, unsigned char* data) {
-    FILE* file = fopen("log.txt", "a");
+int ReadLast(const long handle, unsigned char* data) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "ReadLast param_1: %ld, param_2: %p\n", handle, data);
     fclose(file);
     return 0x137;
@@ -191,7 +182,7 @@ int BlockingReadData(long handle, unsigned char* data, int maxMillis) {
 
 int WriteData(const long handle, const unsigned char* data) {
     if (handle != spoof_handle) {
-        logwrite("Wrong handle");
+        logtext("Wrong handle");
         return -1;
     }
 
@@ -201,7 +192,7 @@ int WriteData(const long handle, const unsigned char* data) {
         const unsigned char led2 = data[3];
         const unsigned char led1 = data[4];
 
-        FILE* file = fopen("leds", "w");
+        FILE* file = fopen(LEDS_FILE, "w");
 
         if (led3 < 128 && led2 < 128 && led1 < 128) {
             fprintf(file, "%c%c%c", led2char(led1), led2char(led2), led2char(led3));
@@ -220,22 +211,22 @@ int WriteData(const long handle, const unsigned char* data) {
 
     }
     else {
-        logwrite("Unknown Command Code");
+        logtext("Unknown Command Code");
         return -1;
     }
 
     return 0;
 }
 
-int FastWrite(long handle, unsigned char* data) {
-    FILE* file = fopen("log.txt", "a");
+int FastWrite(const long handle, unsigned char* data) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "FastWrite param_1: %ld, param_2: %p\n", handle, data);
     fclose(file);
     return 0x191;
 }
 
-int ClearBuffer(long handle) {
-    FILE* file = fopen("log.txt", "a");
+int ClearBuffer(const long handle) {
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "ClearBuffer param_1: %ld\n", handle);
     fclose(file);
     return 0x259;
@@ -289,27 +280,27 @@ int SetErrorCallback(const long handle, const PHIDErrorEvent pErrorCall) {
 }
 
 void DongleCheck2(const int param_1, const int param_2, const int param_3, const int param_4, const int param_5, const int param_6, const int param_7, const uint param_8, const uint *param_9, const uint *param_10, const uint *param_11, const uint *param_12) {
-    FILE* file = fopen("log.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "DongleCheck param_1: %d, param_2: %d, param_3: %d, param_4: %d, param_5: %d, param_6: %d, param_7: %d, param_8: %d, param_9: %d, param_10: %d, param_11: %d, param_12: %d",
         param_1, param_2, param_3, param_4, param_5, param_6, param_7, param_8, *param_9, *param_10, *param_11, *param_12);
     fclose(file);
 }
 
 int SendLEDSausage(const int param_1) {
-    FILE* file = fopen("log.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "SendLEDSausage param_1: %d\n", param_1);
     fclose(file);
     return 0x137;
 }
 
 void CloseInterface(const long handle) {
-    FILE* file = fopen("log.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "CloseInterface param_1: %ld\n", handle);
     fclose(file);
 }
 
 void CleanupInterface(const long handle) {
-    FILE* file = fopen("log.txt", "a");
+    FILE* file = fopen(LOG_FILE, "a");
     fprintf(file, "CleanupInterface param_1: %ld\n", handle);
     fclose(file);
 }
